6058e91564feb6880baeba6c6c9f70eac7181d38
codermonk95 <codermonk95@gmail.com>
2025-04-17 11:44:37 +0530

    first commit

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//A a = new C();
+		A a = () -> System.out.println("inside my method");
+		a.myMethod();
+
+	}
+
+}
