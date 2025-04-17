6631cef3e302d28ebf36d76742b3496102f400fb
codermonk95 <codermonk95@gmail.com>
2025-04-17 12:52:50 +0530

    diamond problem

--- src/com/ms/java8/defaultmethods/B.java
+++ src/com/ms/java8/defaultmethods/B.java
@@ -1,8 +1,12 @@
 package com.ms.java8.defaultmethods;
 
-public class B implements A {
+public class B implements A ,X{
 
 	 public void m1() {
 		System.out.println("m1 inside B");
 	}
+	 
+//	 public void m1() {								if commented then error shows of diamond problem 
+//			System.out.println("m1 inside B");
+//		}
 }
